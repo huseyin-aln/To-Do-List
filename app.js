baf7f426dd2da16d26eb1c4c0fceee1a1aa1bc3b
huseyin <huseyinarslan06@gmail.com>
2022-06-08 15:10:18 +0300

    second step

--- app.js
+++ app.js
@@ -6,23 +6,44 @@ const addBtn = document.getElementById("add-button");
 const toDoList = document.getElementById("to-do-list");
 // console.log(toDoList);
 
-const checkInput = document.createElement("input");
-toDoList.appendChild(checkInput);
-checkInput.type = "checkbox"
-// console.log(checkInput);
-checkInput.id = "check"
 
-const label = document.createElement("label");
-label.setAttribute("for","check");
-toDoList.appendChild(label)
 
 
 addBtn.addEventListener("click", () => {
     if(!toDoInput.value) {
         alert("Please enter something on the to do list!")
+
     } else {
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task-div");
+        toDoList.appendChild(taskDiv);
+
+        const checkInput = document.createElement("input");
+        checkInput.type = "checkbox"
+        checkInput.id = "check"
+        taskDiv.appendChild(checkInput);
+
+        const label = document.createElement("label");
+        label.setAttribute("for","check");
+        taskDiv.appendChild(label);
+
         label.innerHTML += `${toDoInput.value}`;
-        console.log(label.innerHTML);
-        label.innerHTML = "";
-    }
+
+        const taskBtn = document.createElement("button");
+        taskBtn.id = "task-button";
+        taskDiv.appendChild(taskBtn);
+        taskBtn.innerText = "X";
+
+        toDoInput.value = "";
+
+        taskBtn.addEventListener("click", () => {
+            taskDiv.remove()
+        })
+
+        checkInput.addEventListener("click", () => {
+            label.style.textDecoration = "line-through"
+        })
+}
 })
+
+
